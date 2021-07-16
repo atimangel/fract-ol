cb34deff2e1aeff66024b92306336cbbaf4ca948
snpark <snpark@student.42seoul.kr>
2021-07-16 13:17:31 +0900

    맥 norm에 맞춤

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: snpark <snpark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 11:01:51 by snpark            #+#    #+#             */
-/*   Updated: 2021/07/12 12:21:54 by snpark           ###   ########.fr       */
+/*   Updated: 2021/07/16 13:16:51 by snpark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,38 +20,38 @@
 # include <unistd.h>
 # include "mlx.h"
 
-typedef	struct		s_mlx
+typedef struct s_mlx
 {
-	void		*ptr;
-	void		*win;
-	void		*img;
-	void		*pix_str;
-	int		bpp;
-	int		len;
-	int		endian;
+	void			*ptr;
+	void			*win;
+	void			*img;
+	void			*pix_str;
+	int				bpp;
+	int				len;
+	int				endian;
 	unsigned int	x;
 	unsigned int	y;
-	float		ratio;
-	float		max;
-	float		min;
-	float		a;
-	float		b;
-	int		n;
-}			t_mlx;
+	float			ratio;
+	float			max;
+	float			min;
+	float			a;
+	float			b;
+	int				n;
+}				t_mlx;
 
-typedef struct		s_mandelbrot
+typedef struct s_mandelbrot
 {
-	float		ca;
-	float		cb;
-	float		a;
-	float		b;
-	float		aa;
-	float		bb;
+	float			ca;
+	float			cb;
+	float			a;
+	float			b;
+	float			aa;
+	float			bb;
 	unsigned char	r;
 	unsigned char	g;
 	unsigned char	blue;
 	unsigned char	*pixel_byte;
-}			t_mandelbrot;
+}				t_mandelbrot;
 /*
 **EVENT
 */
@@ -60,15 +60,15 @@ int			red_cross(int arg, void *parm);
 /*
 **UTILITIE
 */
-void			error(char *str);
+void		error(char *str);
 int			ft_atoi(const char *string);
-float			ft_atof(const char *string);
+float		ft_atof(const char *string);
 /*
 **MANDELBROT
 */
-float			map(int x, int max, float new_min, float new_max);
-void			put_color(char *pixel, char r, char g, char b);
-void			reset(t_mlx mlx, t_mandelbrot *man, int x, int y);
-void			trans_color(t_mandelbrot *man, int i);
-void			mandelbrot(t_mlx mlx);
+float		map(int x, int max, float new_min, float new_max);
+void		put_color(char *pixel, char r, char g, char b);
+void		reset(t_mlx mlx, t_mandelbrot *man, int x, int y);
+void		trans_color(t_mandelbrot *man, int i);
+void		mandelbrot(t_mlx mlx);
 #endif
